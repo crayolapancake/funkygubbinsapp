bd0c88497fb7bdd36c23f81808592564ec990459
Jemma <jemjo@hotmail.co.uk>
2025-01-26 11:56:58 +0000

    added nb naigation & drawer

--- App.tsx
+++ App.tsx
@@ -1,105 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import './gesture-handler';
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
 import {
   Colors,
-  Header,
 } from 'react-native/Libraries/NewAppScreen';
+import Home from './src/Home';
+import Shop from './src/Shop';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({ children, title }: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const Drawer = createDrawerNavigator();
 
-  const backgroundStyle = {
+  const statusBarBackgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const scrollViewBackgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.black : Colors.white,
+  };
+
+  // redux / saga for state management
+  // context api
+  // implement typescript
+  // add unit tests
+  // add local storage (faves list)
+  // etsy api
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Funky Gubbins">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          drawerActiveTintColor: '#03f4fc',
+          drawerLabelStyle: {
+            color: Colors.dark,
+            fontSize: 20,
+            // fontFamily: 'Georgia',
+          },
+        }}
+      >
+        <Drawer.Screen name="Home" component={Home} options={{ title: 'Funky Gubbins' }} />
+        <Drawer.Screen name="Shop" component={Shop} options={{ title: 'Shop' }} />
+        {/* <Drawer.Screen name="About Us" component={AboutUs} options={{ title: 'About' }}/> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+
+
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- android/app/src/main/java/com/funkygubbins/MainActivity.kt
+++ android/app/src/main/java/com/funkygubbins/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.funkygubbins
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +20,13 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+ 
+
+  /**
+   * Required for RN navigation: to avoid crashes related to View state being not persisted consistently across Activity 
+   * restarts.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
